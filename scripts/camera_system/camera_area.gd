5989606bfd1dc4d7350b53b620ed5adfe1b5204b
karjon7 <80655086+karjon7@users.noreply.github.com>
2024-09-29 23:02:46 -0400

    Add camera switching

--- scripts/camera_system/camera_area.gd
+++ scripts/camera_system/camera_area.gd
@@ -1,2 +1,21 @@
 extends Area3D
 class_name CameraArea
+
+@export var assigned_camera : Camera3D
+
+func _ready():
+	if not assigned_camera: push_error("No Camera Assigned")
+	pass #body_entered.connect(_player_entered)
+
+
+func _process(delta):
+	if has_overlapping_bodies():
+		assigned_camera.make_current()
+
+
+func _player_entered(_body):
+	if not assigned_camera: 
+		push_error("No Camera Assigned")
+		return
+	
+	assigned_camera.make_current()
--- scripts/facility/hallway_piece.gd
+++ scripts/facility/hallway_piece.gd
@@ -2,9 +2,27 @@ extends Node3D
 class_name HallwayPiece
 
 @export var connection_markers : Array[ConnectionMarker]
-@export var clearance_area : Area3D
+@export var cam_configs_holder : Node3D
+@export var clearance_area : StaticBody3D
 
 
-func is_placement_clear() -> bool:
-	var is_cleared = !clearance_area.has_overlapping_bodies()
-	return is_cleared
+func _ready():
+	assert(connection_markers, "No Connection Markers")
+	assert(clearance_area, "No Clearance Area")
+	
+	_check_for_cam_configs()
+
+
+func _check_for_cam_configs():
+	if not cam_configs_holder: 
+		push_error("No Cam Config Holder")
+		return
+	
+	var atleast_one_config = false
+	
+	for child in cam_configs_holder.get_children():
+		if child is CameraConfiguration: 
+			atleast_one_config = true
+			break
+	
+	if not atleast_one_config: push_error("No Camera Configs")
