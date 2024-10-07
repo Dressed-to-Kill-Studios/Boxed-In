624389b4b206a0f86817eae4424c33bfcb9cc5ac
karjon7 <80655086+karjon7@users.noreply.github.com>
2024-10-06 21:27:59 -0400

    Added facility door and changes to safe room and hallway pieces

--- scripts/interactable/interactable.gd
+++ scripts/interactable/interactable.gd
@@ -5,12 +5,6 @@ class_name Interactable
 @export var can_interact_messages : Array[String] = ["I can interact with this."]
 @export var cant_interact_messages : Array[String] = ["I can't interact with this"]
 
-@export_group("Required")
-@export var interact_area : CollisionShape3D
-
-func _ready():
-	assert(interact_area, "No Interact Area")
-
 
 func interact(interactor : CharacterBody3D) -> String:
 	var interact_msg : String = can_interact_messages[randi() % can_interact_messages.size()]
