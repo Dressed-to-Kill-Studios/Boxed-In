e68a334c5358552f87e50e5fb2dc035f436ed9dc
Jon-Marc Redwood <karjon.redwood@gmail.com>
2024-11-02 16:56:45 -0400

    Update AudioManager

--- scenes/game/game.gd
+++ scenes/game/game.gd
@@ -15,8 +15,9 @@ var current_difficulty : DIFFICULTIES
 func _init(_difficulty : DIFFICULTIES = DIFFICULTIES.NORMAL):
 	current_difficulty = _difficulty
 
+
 func _ready():
 	if Engine.is_editor_hint(): return
 	
-	AudioManager.play(AudioManager.AUDIO_THEMES.SAFE_ROOM)
+	AudioManager.play(AudioManager.AUDIO_THEMES.AMBIENCE)
 	facility.generate_facility()
--- scripts/static/audio_manager.gd
+++ scripts/static/audio_manager.gd
@@ -1,5 +1,8 @@
 extends Node
 
+
+signal theme_changed(old_theme : AUDIO_THEMES, new_theme : AUDIO_THEMES)
+
 enum AUDIO_THEMES {
 	TITLE,
 	SAFE_ROOM,
@@ -24,24 +27,38 @@ const AUDIO = {
 		preload("res://assets/audio/ambience/Ambience_4.mp3"),
 		preload("res://assets/audio/ambience/Ambience_5.mp3"),
 	],
-	"chase" : [],
-	"game_over" : [],
+	"chase" : [
+		preload("res://assets/audio/music/chase/Vision.wav")
+	],
+	"game_over" : [
+		preload("res://assets/audio/music/game_over/Channel.wav"),
+		preload("res://assets/audio/music/game_over/Memory.wav"),
+		preload("res://assets/audio/music/game_over/Ruin.wav"),
+	],
 }
 
-var current_audio : AudioStreamPlayer
+var current_player : AudioStreamPlayer
+var current_theme : AUDIO_THEMES
 
 
 func _ready() -> void:
 	pass
 
 
+func change_theme(audio_theme : AUDIO_THEMES) -> void:
+	theme_changed.emit(current_theme, audio_theme)
+	
+	current_theme = audio_theme
+
+
 func play(audio_theme : AUDIO_THEMES) -> void:
-	if current_audio: current_audio.queue_free()
+	if current_player: current_player.queue_free()
 	
-	current_audio = AudioStreamPlayer.new()
-	add_child(current_audio)
+	current_player = AudioStreamPlayer.new()
+	add_child(current_player)
 	
-	var chosen_theme : Array = AUDIO.get(AUDIO.keys()[audio_theme])
+	change_theme(audio_theme)
+	var chosen_theme : Array = AUDIO.get(AUDIO.keys()[current_theme])
 	
-	current_audio.stream = chosen_theme[randi() % chosen_theme.size()]
-	current_audio.play()
+	current_player.stream = chosen_theme[randi() % chosen_theme.size()]
+	current_player.play()
